7d1c904d6a261b693071ff4bd7d511059f0a5178
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-05 12:18:05 +0530

    input program

---
+++ input.py
@@ -0,0 +1,19 @@
+# simple input program
+
+a = input()
+print(a)
+
+
+# convert into in interger value
+
+a = input("Enter a number:")
+b = input("Enter a second number:")
+
+print(int(a) + int(b))
+
+# you can initialize directly like that
+
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+
+print("the multiplication  of number is ",a*b , "=")
