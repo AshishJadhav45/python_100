8582008a011229e3a7d7165535ee57552c3dab95
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-05 12:17:30 +0530

    calcy

---
+++ calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a first number:"))
+b = int(input("Enter a second number:"))
+
+print("the vlaue of ", a , "+" , b , "is =", a+b)
+print("the vlaue of ", a , "-" , b , "is =", a-b)
+print("the vlaue of ", a , "*" , b , "is =", a*b)
+print("the vlaue of ", a , "/" , b , "is =", a/b)
