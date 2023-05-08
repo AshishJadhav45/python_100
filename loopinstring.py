a6f08269ab0092c7e394f12e94e361e9c1f395a8
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-08 23:35:52 +0530

    programs

---
+++ loopinstring.py
@@ -0,0 +1,34 @@
+'''fruits = "Muskmelon"
+for i in fruits:
+    print(i)
+'''
+
+fruits = "Apple"
+print(fruits.upper())
+print(fruits.lower())
+
+# rstrip():
+''' The rstrip removes any tailing character Example'''
+
+str = "Anderson !!"
+print(str.rstrip("!"))
+
+#center()
+''' The center() method aligns the string to the center as the parameter given by the user'''
+
+System = "Welcome to console!"
+print(System.center(50, "."))
+
+
+# Endswith()
+
+os = "Real time operating system"
+print(os.endswith("system"))
+
+# index 
+'''The index method searches for the first occurance of the given value and return the index wher it is present'''
+
+str1 = "He's name is neo !. he is an honest man."
+print(str1.index("neo"))
+
+
