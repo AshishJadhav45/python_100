a145b07d42a2a23dd7dd76eb73487fcae7c9d7e9
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-09 01:34:58 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,15 @@
+name = "Abhishek"
+
+for i in name:
+    print(i, end = ",")
+
+
+
+
+color = ["green","white", "pink", "yellow"]
+for x in color:
+    print(x)
+
+
+for k in range(2,59):
+    print(k)
