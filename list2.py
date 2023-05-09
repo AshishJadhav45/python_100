6d6c01f884bf2a70192fd2fa39c426774b5d9846
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-10 00:53:24 +0530

    list Program

---
+++ list2.py
@@ -0,0 +1,8 @@
+color = ["yellow","green","pink","white","blue"]
+if "pink" in color:
+    print("pink is there")
+
+print(len(color))
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[::2])		#using positive indexes
