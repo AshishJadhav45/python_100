9ac3f56996fb0b5bf6bdc609b5df8d2eebebb5c6
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-16 23:09:14 +0530

    delete program

---
+++ del.py
@@ -0,0 +1,11 @@
+
+player = {"Virat","Rohit","M S dhoni","A B de villers","Sachin"}
+# del player 
+
+# print(player)
+# dictionary are unorder
+# del function delet all the item that present in function 
+if "Virat" in player:
+    print("virat is in player list")
+else :
+    print("virat is not player list")
