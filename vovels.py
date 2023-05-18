a2dcd4818397d46aaa01f430ce07155b0d24c6b3
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-18 19:48:38 +0530

    vovels.py

---
+++ vovels.py
@@ -0,0 +1,14 @@
+def count_vowels(string):
+    vowels = 'aeiouAEIOU'
+    count = 0
+
+    for char in string:
+        if char in vowels:
+            count += 1
+
+    return count
+
+# Example usage
+input_string = input("Enter a string: ")
+vowel_count = count_vowels(input_string)
+print("Number of vowels:", vowel_count)
