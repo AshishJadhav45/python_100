5f0f577b47ebfe4917c4242ca920eb5c926c7611
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-30 20:05:28 +0530

    Enumerate_function.py

---
+++ Enumerate_function.py
@@ -0,0 +1,18 @@
+
+# example of enumerate
+
+player = ["virat","rohit","dhoni","sachine"]
+for index, player in enumerate(player):
+    print(index,player)
+
+# and here it is example with string
+
+s = "patric"
+for index , s in enumerate(s):
+    print(index,s)
+
+
+
+color = ["red","pink","yellow","black"]
+for index, color in enumerate(color):
+    print(index,color)
