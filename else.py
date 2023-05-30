bb4e7a7849be73928376af9aa3006d223af425c1
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-30 20:07:27 +0530

    if else in one line

--- else.py
+++ else.py
@@ -3,3 +3,15 @@ for x in range(5):
 else:
     print('else block in loop ')
 print("Out of loop!")
+
+
+# if ..else in one line 
+
+# a = 3
+# b = 330
+# print("A") if a > b else print("B")
+
+
+a = 45
+b = 5
+print("A") if a > b else print("B")
