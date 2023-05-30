41deb42f6fb8dd5aaf1ff200f1db69b0307327b8
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-30 20:06:06 +0530

    finally function program

---
+++ finally.py
@@ -0,0 +1,8 @@
+try:
+    num = int(input("Enter a interger:"))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("integer Accepted.")
+finally:
+    print("this is always executed.")
