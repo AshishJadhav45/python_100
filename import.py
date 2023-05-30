c4e1622cc6e705670bba71899c9c4b95a7ac4f2e
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-30 20:06:57 +0530

    import function programs

---
+++ import.py
@@ -0,0 +1,26 @@
+# import example
+# import math 
+
+# result = math.sqrt(9)
+# print(result)
+
+# From keyword example
+"""
+from math import sqrt
+
+result = sqrt(9)
+print(result)
+"""
+# you can also import multiple functions
+
+from math import sqrt,pi
+
+result=sqrt(7)
+print(result)
+print(pi)
+
+# you can use as
+import math as m
+
+rs = m.sqrt(3)
+print(rs)
