f85e9d5fc0ddd01eb697ee6b82b95e1dc5a306a2
AshishJadhav <vinodjadhav1845@gmail.com>
2023-05-30 19:07:55 +0530

    custom_erroro program

--- custom_error.py
+++ custom_error.py
@@ -1,13 +1,18 @@
-salary = int(input("Enter salary amout:"))
+# salary = int(input("Enter salary amout:"))
 
-if not 2000 < salary < 5000:
-    raise ValueError("Not a valid salary!")
+# if not 2000 < salary < 5000:
+#     raise ValueError("Not a valid salary!")
 
-# Defining Custom Exception 
+# # Defining Custom Exception 
 
-# this is example of defingin custom Exception
+# # this is example of defingin custom Exception
 
-    class CustomError(Exception):
-        pass
-try:
- except CustomError:
+#     class CustomError(Exception):
+#         pass
+# try:
+# # except CustomError:
+try: 
+    num = int(input("Enter a number!"))
+except ValueError:
+    print("Number enter is not a interger!")
+
