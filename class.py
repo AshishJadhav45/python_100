80180017dbc94b4c3273ae6f54f023cda2460a37
AshishJadhav <vinodjadhav1845@gmail.com>
2023-06-05 11:55:41 +0530

    class program

---
+++ class.py
@@ -0,0 +1,14 @@
+class stud:
+    def numprint(self):
+        print(self.nm)
+        print("Example of charactor")
+
+    # default costructor
+
+    def __init__ (self):
+        self.nm = "Neo"
+
+s = stud()
+
+
+s.numprint()
