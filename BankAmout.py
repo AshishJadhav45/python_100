b0f94df6f3f027478edee73e5a075f8ae4fc2349
AshishJadhav <vinodjadhav1845@gmail.com>
2023-06-06 14:13:31 +0530

    Bank amount program

---
+++ BankAmout.py
@@ -0,0 +1,24 @@
+#write python program to simulate a bank amount for deposit , withdraw amount 
+#operation 
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposit of ${amount} successful. Current balance: ${self.balance}")
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"Withdrawal of ${amount} successful. Current balance: ${self.balance}")
+        else:
+            print("Insufficient funds. Withdrawal canceled.")
+
+# Creating an instance of the BankAccount class
+account = BankAccount()
+
+# Test the program
+account.deposit(1000)  # Deposit $1000
+account.withdraw(500)  # Withdraw $500
+account.withdraw(700)  # Withdraw $700 (Insufficient funds)
